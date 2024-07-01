2a5120a912d9c814f576948891431f6be0a8711c
Anirudh0030 <chaubeanirudh@gmail.com>
2024-07-01 21:40:38 +0530

    updated day 8,9

---
+++ DAY9 Dictinory & Nesting/main.py
@@ -0,0 +1,38 @@
+# # programming_dictionary = {
+# #     "Bug": "An error in a program that prevents the program from running as expected.", 
+# #     "Function": "A piece of code that you can easily call over and over again."
+# #     }
+
+# #{KEY : VALUE}
+
+# #to retrive the information from dicitonary
+
+# #print(programming_dictionary["BUG"])
+
+# # #adding new items to dictionary
+
+# # progra_dicti["LOOP"] = "This is the action"
+# # print(progra_dicti)
+
+# # #creating a empty dic{}
+# # empty_dictionary = {}
+# # empty_dictionary["loop"] = "This is newley added"
+# # print(empty_dictionary)
+
+# #wiping an existing dic
+
+# # programing_dic = {}
+# # print(progra_dicti)
+
+# # # edit and time in a dictionary
+# prog_dic ={
+#      "BUG" : "Hi there" 
+#  }
+# # prog_dic["BUG"] = "we found a bug"
+# # print(prog_dic)
+
+
+# #loop through a dic (IT just give us key)
+
+# for thing in prog_dic:
+#     print(thing)
